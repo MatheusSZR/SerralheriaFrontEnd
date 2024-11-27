4b6514e5806cc532688b7a109a27fb71ec59a422
MatheusSZR <matheusdesouzarodrigues34@gmail.com>
2024-11-27 10:47:10 -0300

    ATUALIZADO!!!

--- js/script.js
+++ js/script.js
@@ -1,20 +1,27 @@
-var modal = document.getElementById("myModal");
-var btn = document.getElementById("myBtn");
+// Script para modal de cadastro de orçamento
+// Obtém os elementos do modal, botão de abrir e o botão de fechar
+var modal = document.getElementById("cadastroModal");
+var btn = document.getElementById("openModalBtn");
 var span = document.getElementsByClassName("close")[0];
 
+// Ao clicar no botão de "Cadastrar Orçamento", abre o modal
 btn.onclick = function() {
-    modal.style.display = "block";
+    modal.style.display = "flex"; // Torna o modal visível
 }
 
+// Fechar o modal ao clicar no 'X'
 span.onclick = function() {
-    modal.style.display = "none";
+    modal.style.display = "none"; // Esconde o modal
 }
 
+// Fechar o modal clicando fora dele
 window.onclick = function(event) {
     if (event.target == modal) {
-        modal.style.display = "none";
+        modal.style.display = "none"; // Esconde o modal
     }
 }
+
+// Script para o efeito de zoom nas imagens da galeria
 document.querySelectorAll('.zoom').forEach(item => {
     item.addEventListener('click', function() {
         this.style.transform = 'scale(1.5)';
